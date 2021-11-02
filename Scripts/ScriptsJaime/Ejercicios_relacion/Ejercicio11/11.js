2eacd5b09cbe976afe0ab5082507c7bb735fea2e
Carlitosaguilaar <caa0005@alu.medac.es>
2021-11-02 11:27:31 +0100

    Arreglos 02/11

--- Scripts/ScriptsJaime/Ejercicios_relacion/Ejercicio11/11.js
+++ Scripts/ScriptsJaime/Ejercicios_relacion/Ejercicio11/11.js
@@ -1,11 +1,9 @@
 function dos_aparece() {
     var dedo = document.getElementById("div2");
-    dedo.style.visibility="visible";
     dedo.style.display="block";
 }
 
 function dos_desaparece() {
     var dedo = document.getElementById("div2");
-    dedo.style.visibility="hidden";
     dedo.style.display="none";
 }
