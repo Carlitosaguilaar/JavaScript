d47f682edaf6bbe6d28977365331687479c51427
Carlitosaguilaar <caa0005@alu.medac.es>
2021-11-18 14:39:34 +0100

    Avances de noseque

--- Scripts/ScriptsJaime/Ejercicios_relacion/Acceso_a_Nodos/Ejercicio7/7.js
+++ Scripts/ScriptsJaime/Ejercicios_relacion/Acceso_a_Nodos/Ejercicio7/7.js
@@ -1,21 +1,38 @@
 window.onload = function () {
     var tabla = document.getElementById("tabla");
     tabla.setAttribute=("backgroundColor", "white");
-    tabla.setAttribute=("Width", "400px");
-    tabla.setAttribute=("Height", "400px");
+    tabla.setAttribute=("Width", "100px");
+    tabla.setAttribute=("Height", "100px");
     tabla.setAttribute=("border", "2px");
 
 
     var celdas = document.getElementsByTagName("td");
+    var colores = ["blue", "red", "orange", "yellow"];
     for (let i=0; i<celdas.length; i++){
-            celdas[i].onmouseover=function(){                     
+            celdas[i].onclick=function(){                     
 
             if (this.style.backgroundColor == "" || this.style.backgroundColor == "white") {
                 this.style.backgroundColor = "black";
 
             }
             else if (this.style.backgroundColor == "black") {
-                    this.style.backgroundColor = "white"       
+                this.style.backgroundColor = colores[0];       
+            }
+
+            else if (this.style.backgroundColor == colores[0]) {
+                this.style.backgroundColor = colores[1];
+            }
+
+            else if (this.style.backgroundColor == colores[1]) {
+                this.style.backgroundColor = colores[2];
+            }
+
+            else if (this.style.backgroundColor == colores[2]) {
+                this.style.backgroundColor = colores[3];
+            }
+
+            else{
+                this.style.backgroundColor =colores[0];
             }
         }
     }
