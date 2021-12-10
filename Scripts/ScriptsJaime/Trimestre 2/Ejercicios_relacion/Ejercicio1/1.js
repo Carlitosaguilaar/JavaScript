7b2411e7d8227724d6a5387d8b2997bc83813b52
Carlitosaguilaar <caa0005@alu.medac.es>
2021-12-10 13:59:35 +0100

    Avance ejercicio 1

---
+++ Scripts/ScriptsJaime/Trimestre 2/Ejercicios_relacion/Ejercicio1/1.js
@@ -0,0 +1,16 @@
+function comprobante(lista, valores) {
+    var lista = [1,2,3,4,5,6,7,8,9];
+
+    valores = prompt ("Dime el número que quieres comprobar que se encuentra en el Array")
+
+    
+    for (let i = 0; i <= lista.length; i++){
+        if (valores == lista[i]) {
+            return "Verdadero";
+        }
+    }
+    return "Falso";
+}
+
+    alert (comprobante())
+
