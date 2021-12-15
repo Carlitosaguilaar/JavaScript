d6840e6ac8011f4406539580fc52ffa4fd4d2921
Carlitosaguilaar <caa0005@alu.medac.es>
2021-12-15 14:44:31 +0100

    Merge branch 'master' of https://github.com/Carlitosaguilaar/JavaScript

--- Scripts/ScriptsJaime/Trimestre 2/Ejercicios_relacion/Objetos/Alumno/Alumno.js
+++ Scripts/ScriptsJaime/Trimestre 2/Ejercicios_relacion/Objetos/Alumno/Alumno.js
@@ -1,54 +1,54 @@
 //Ejercicio 1
 
-var tutor1 = {
+var t1 = {
     nombre: "Yeimi",
     edad: 40,
     dni: "12345678P",
     titulo_universitario: "ing_software"        
 }
 
-var tutor2 = {
+var t2 = {
     nombre: "ErFasilito",
     edad: 80,
     dni: "11122233F",
     titulo_universitario: "GradoMedio"        
 }
 
-var asignatura1 = {
+var a1 = {
     nombre:"Clientes",
     curso: 2, 
     horas: 6
 }
 
-var asignatura2 = {
+var a2 = {
     nombre:"Servidores",
     curso: 2,
     horas: 8
 }
 
-var asignatura3 = {
+var a3 = {
     nombre:"Interfaces",
     curso: 2,
     horas: 8
 }
 
-var asignatura4 = {
+var a4 = {
     nombre:"Despliegue",
     curso: 2,
     horas: 3
 }
 
 
-var asignaturas = [asignatura1, asignatura2, asignatura3, asignatura4]
-var tutores = [tutor1, tutor2]
+var a = [a1, a2, a3, a4]
+var t = [t1, t2]
 
 var alumno = {
     nombre: "Carlos", 
     edad: 21,
     ciclo :"daw",
     curso: 2,
-    tutor: tutores,
-    asignatura: asignaturas,
+    tutor: t,
+    asignatura: a,
     notamedia: 5
     
 }
@@ -62,11 +62,68 @@ window.onload = function pantalla() {
     dedo2.innerHTML = muestradatos();
     
         
-    function muestradatos() {        
-        alumno.forEach(element => {
-            
-        });
+    
+document.body.appendChild(caja)
+}
+
+//Ejercicio 3
+
+
+function mostrar() {
+    var numprofe = prompt ("Hay dos profesores, de cual quieres obtener toda la información? Introduce el numero (1- Yeimi, 2- ErFasilito)");
+    if (numprofe == 1) {
+
+        
+        
+    }
+    else if (numprofe ==2){
+
     }
 
-document.body.appendChild(caja)
+    
+    var cadena = ""
+    
+}
+
+
+
+function cambiarNombre(nuevonombre) {
+    var numprofe = prompt ("Hay dos profesores, de cual quieres cambiar el nombre? Introduce el numero. 1- "+t1.nombre+", 2- "+t2.nombre);
+    var nombreantiguo = "";
+
+        if (numprofe == 1) {
+            let nuevonombre = prompt ("¿Cuál es el nuevo nombre que quieres ponerle a "+t1.nombre+"?");
+            nombreantiguo = t1.nombre;
+            t1.nombre = nuevonombre;
+            alert ("El nombre ha sido cambiado de "+nombreantiguo+ " a "+nuevonombre)
+
+        }
+        else if (numprofe == 2) {
+            let nuevonombre = prompt ("¿Cuál es el nuevo nombre que quieres ponerle a "+t2.nombre+"?");
+            nombreantiguo = t2.nombre;
+            t2.nombre = nuevonombre;
+            alert ("El nombre ha sido cambiado de "+nombreantiguo+ " a "+nuevonombre)
+     
+        }
+
+
+    
+}
+
+//Ejercicio 4
+
+// cambiarHoras(nueva): permite cambiar el número de horas del objeto.
+
+function cambiarHoras() {
+    var numasignatura = prompt ("De qué asignatura quieres modificar el número de horas? (Inserta el número: 1-"+a1.nombre+", 2-"+ a2.nombre+", 3-"+a3.nombre+", 4-"+a4.nombre+")")
+    switch (numasignatura) {
+        case 1:
+            let nuevashoras = prompt ("Cual es el nuevo numero de horas?")
+            
+            break;
+    
+        default:
+            break;
+    }
+    
 }
