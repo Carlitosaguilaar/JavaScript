7ea77c763f7c85031edd33b5715ce98f3f058215
Carlitosaguilaar <caa0005@alu.medac.es>
2021-12-16 14:38:46 +0100

    16/12

--- Scripts/ScriptsJaime/Trimestre 2/Ejercicios_relacion/Objetos/Alumno/Alumno.js
+++ Scripts/ScriptsJaime/Trimestre 2/Ejercicios_relacion/Objetos/Alumno/Alumno.js
@@ -1,129 +1,185 @@
 //Ejercicio 1
+var tutores = [
+    {
+        nombre: "Javi",
+        edad: 38,
+        dni: "12345678B",
+        titulo_uni: "Ingeniería electrica"
+    },
+    {
+        nombre: "Jaime",
+        edad: 40,
+        dni: "87654321C",
+        titulo_uni: "Ingeniería de software"
+    }
+]
 
-var t1 = {
-    nombre: "Yeimi",
-    edad: 40,
-    dni: "12345678P",
-    titulo_universitario: "ing_software"        
-}
-
-var t2 = {
-    nombre: "ErFasilito",
-    edad: 80,
-    dni: "11122233F",
-    titulo_universitario: "GradoMedio"        
-}
-
-var a1 = {
-    nombre:"Clientes",
-    curso: 2, 
-    horas: 6
-}
-
-var a2 = {
-    nombre:"Servidores",
-    curso: 2,
-    horas: 8
-}
-
-var a3 = {
-    nombre:"Interfaces",
-    curso: 2,
-    horas: 8
-}
-
-var a4 = {
-    nombre:"Despliegue",
-    curso: 2,
-    horas: 3
-}
-
-
-var a = [a1, a2, a3, a4]
-var t = [t1, t2]
+var asignaturas = [ 
+    {
+        nombre: "Desarrollo en entorno de servidores",
+        curso: 2,
+        horas: 300
+    },
+    {
+        nombre: "Desarrollo en entorno de clientes",
+        curso: 2,
+        horas: 300
+    },
+    {
+        nombre: "Despliegue de aplicaciones web",
+        curso: 2,
+        horas: 150
+    },
+    {
+        nombre: "Diseño de interfaces",
+        curso: 2,
+        horas: 300
+    }
+]
 
 var alumno = {
     nombre: "Carlos", 
     edad: 21,
-    ciclo :"daw",
+    ciclo: "daw",
     curso: 2,
-    tutor: t,
-    asignatura: a,
-    notamedia: 5
+    tutor: tutores,
+    asignatura: asignaturas,
+    notas: [6.75,6,8,10]
     
 }
 
 //Ejercicio 2
 window.onload = function pantalla() {
-    var dedo = document.querySelector("body")
     var caja = document.createElement("div")
-    dedo.appendChild(caja)
-    var dedo2 = document.querySelector("div")
-    dedo2.innerHTML = muestradatos();
-    
+    document.querySelector("body").appendChild(caja)
+
+    for (let campo in alumno){
+        let nuevoSpan = document.createElement("span")
+        let texto;
+
+        switch (campo) {
+            case "tutor":
+                texto ="";
+                for (let datos in alumno[campo]) {
+                    texto += alumno[campo][datos]+" "; 
+                }
+                break;
+
+            case "asignatura":
+                for (let ind of alumno[campo]){ 
+                    texto += ind.nombre+" ";
+                }
+                break;
+            
+            case "notas":
+                let suma=0;
+                for (let ind of alumno[campo]){
+                    suma+=ind
+                }
+                texto = "La media es: "+(suma/alumno[campo].length);
+                break;
+            default:
+                
+                texto = document.createTextNode(alumno[campo]);
+        }
+
+        let nodoTexto = document.createTextNode(texto);
         
-    
-document.body.appendChild(caja)
+        nuevoSpan.appendChild(nodoTexto)
+        caja.appendChild(nuevoSpan)
+    }
 }
 
 //Ejercicio 3
 
 
 function mostrar() {
-    var numprofe = prompt ("Hay dos profesores, de cual quieres obtener toda la información? Introduce el numero (1- Yeimi, 2- ErFasilito)");
+    var numprofe = prompt ("Hay dos profesores, de cual quieres obtener toda la información? Introduce el numero (1- Jaime, 2- Javi)");
     if (numprofe == 1) {
 
-        
-        
+       
     }
     else if (numprofe ==2){
 
     }
-
-    
-    var cadena = ""
-    
+    var cadena = ""    
 }
 
-
-
 function cambiarNombre(nuevonombre) {
-    var numprofe = prompt ("Hay dos profesores, de cual quieres cambiar el nombre? Introduce el numero. 1- "+t1.nombre+", 2- "+t2.nombre);
+    var numprofe = prompt ("Hay dos profesores, de cual quieres cambiar el nombre? Introduce el numero. 1- "+tutores[0].nombre+", 2- "+tutores[1].nombre);
     var nombreantiguo = "";
 
         if (numprofe == 1) {
-            let nuevonombre = prompt ("¿Cuál es el nuevo nombre que quieres ponerle a "+t1.nombre+"?");
-            nombreantiguo = t1.nombre;
-            t1.nombre = nuevonombre;
+            let nuevonombre = prompt ("¿Cuál es el nuevo nombre que quieres ponerle a "+tutores[0].nombre+"?");
+            nombreantiguo = tutores[0].nombre;
+            tutores[0].nombre = nuevonombre;
             alert ("El nombre ha sido cambiado de "+nombreantiguo+ " a "+nuevonombre)
 
         }
         else if (numprofe == 2) {
-            let nuevonombre = prompt ("¿Cuál es el nuevo nombre que quieres ponerle a "+t2.nombre+"?");
-            nombreantiguo = t2.nombre;
-            t2.nombre = nuevonombre;
+            let nuevonombre = prompt ("¿Cuál es el nuevo nombre que quieres ponerle a "+tutores[1].nombre+"?");
+            nombreantiguo = tutores[1].nombre;
+            tutores[1].nombre = nuevonombre;
             alert ("El nombre ha sido cambiado de "+nombreantiguo+ " a "+nuevonombre)
      
         }
 
+}
+//Ejercicio 4
 
+function mostrar() {
     
 }
 
-//Ejercicio 4
-
-// cambiarHoras(nueva): permite cambiar el número de horas del objeto.
-
 function cambiarHoras() {
-    var numasignatura = prompt ("De qué asignatura quieres modificar el número de horas? (Inserta el número: 1-"+a1.nombre+", 2-"+ a2.nombre+", 3-"+a3.nombre+", 4-"+a4.nombre+")")
-    switch (numasignatura) {
-        case 1:
-            let nuevashoras = prompt ("Cual es el nuevo numero de horas?")
-            
-            break;
-    
-        default:
-            break;
+    var numasignatura = 0;
+    numasignatura = prompt ("De qué asignatura quieres modificar el número de horas? (Inserta el número: 1-"+asignaturas[0].nombre+", 2-"+ asignaturas[1].nombre+", 3-"+asignaturas[2].nombre+", 4-"+a4.nombre+")")
+    var horasantiguas = 0;
+    var nuevashoras = 0;
+
+    if (numasignatura == 1) {
+        nuevashoras = prompt ("Cual es el nuevo numero de horas?");
+        horasantiguas = asignaturas[0].horas;
+        asignaturas[0].horas = nuevashoras;
+        alert ("Las horas de la asignatura "+asignaturas[0].nombre+" han cambiado de "+horasantiguas+" a "+nuevashoras)
     }
+
+    else if (numasignatura == 2) {
+        nuevashoras = prompt ("Cual es el nuevo numero de horas?");
+        horasantiguas = asignaturas[1].horas;
+        asignaturas[1].horas = nuevashoras;
+        alert ("Las horas de la asignatura "+asignaturas[1].nombre+" han cambiado de "+horasantiguas+" a "+nuevashoras)
+    }
+    else if (numasignatura == 3) {
+        nuevashoras = prompt ("Cual es el nuevo numero de horas?");
+        horasantiguas = asignaturas[2].horas;
+        asignaturas[2].horas = nuevashoras;
+        alert ("Las horas de la asignatura "+asignaturas[2].nombre+" han cambiado de "+horasantiguas+" a "+nuevashoras)
+    }
+    else if (numasignatura == 4){
+        nuevashoras = prompt ("Cual es el nuevo numero de horas?");
+        horasantiguas = a4.horas;
+        a4.horas = nuevashoras;
+        alert ("Las horas de la asignatura "+asignaturas[3].nombre+" han cambiado de "+horasantiguas+" a "+nuevashoras)
+    } else{
+        alert ("ERROR, pulse el botón otra vez introduciendo un número válido")
+    }
+}
+    
+//Ejercicio 5
+
+function calcularMedia() {
+    var notafinal = 0;
+    alert ("Vamos a calcular la nota final del alumno "+alumno.nombre);
+    notafinal = (alumno.notas[0] + alumno.notas[1] + alumno.notas[2] + alumno.notas[3]) / 4;
+
+    alert ("La nota media de "+alumno.nombre+" es "+notafinal)
+}
+
+function MediaAsignatura() {
+
+}
+
+function mostrar() {
     
 }
---
+++ Scripts/ScriptsJaime/Trimestre 2/Ejercicios_relacion/Objetos/Usuario/Usuario.js
@@ -0,0 +1,49 @@
+var genre = {
+    genero: "masculino"
+}
+
+var datos = {
+    name: "Carlos", 
+    lastname: "Aguilar",
+    age: 21,
+    dni: "12312312V"
+}
+
+var address = {
+    street: "Av. Micasa",
+    number:  "3",
+    city: "Malaga",
+    country: "España"
+}
+
+var coordenates = {
+    latitude : "",
+    longitude : ""
+}
+
+var login = {
+    id: "11122233341234",
+    User: "Carlitoos",
+    Pass: "SoyImbecil",
+    Picture: ""
+}
+
+var contact = {
+    phone: "601601601",
+    email: "caa0005@alu.medac.es",
+    SSNN: ["instagram.com/carlitosaguilaar", "instagram.com/carlitosaguilaar", "instagram.com/carlitosaguilaar"],
+}
+
+var pictures = {
+    big: ["","",""],
+    small: ["","",""]
+}
+
+var softwareSkills = {
+    programas : ["","","","",""]
+}
+
+var usuario = {
+    genre: genero[0],
+
+}
